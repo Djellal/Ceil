d3ae61ad47db19e999cc08af36f77823fcda5951
Djellal Elkacem <djellal.elkacem@gmail.com>
2025-07-11 15:57:44 +0200

    Ajoutez des fichiers projet.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Ceil.Models;
+using DataModel;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ceil.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Session> Sessions { get; set; }
+    public DbSet<AppSetting> AppSettings { get; set; }
+    public DbSet<CourseType> CourseTypes { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<CourseLevel> CourseLevels { get; set; }
+    public DbSet<CourseComponent> CourseComponents { get; set; }
+    public DbSet<State> States { get; set; }
+    public DbSet<Municipality> Municipalities { get; set; }
+    public DbSet<CourseRegistration> CourseRegistrations { get; set; }
+    public DbSet<Profession> Professions { get; set; }
+    public DbSet<Groupe> Groupes { get; set; }
+}
---
+++ Models/Groupe.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ceil.Models
+{
+    public class Groupe
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(30)]
+        public string GroupeName { get; set; } = "";
+
+        [Required]
+        [ForeignKey("Course")]
+        public int CourseId { get; set; }
+        public Course? Course { get; set; }
+
+        [Required]
+        [ForeignKey("CourseLevel")]
+        [Display(Name = "Course Level")]
+        public int CourseLevelId { get; set; }
+        public CourseLevel? CourseLevel { get; set; }
+
+        [Required]
+        [ForeignKey("Session")]
+        public int SessionId { get; set; }
+        public Session? Session { get; set; }
+    }
+}
